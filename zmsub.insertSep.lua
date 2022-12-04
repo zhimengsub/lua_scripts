e275f6505501901b4be963faa3be0d52e06b7ce2
barryZZJ <850462618@qq.com>
2022-12-04 14:10:28 +0800

    match \+N+ raise error if multiple \N refine line number in error log

--- zmsub.insertSep.lua
+++ zmsub.insertSep.lua
@@ -3,34 +3,54 @@ local tr = aegisub.gettext
 script_name = tr"插入日字特效 (选中行)"
 script_description = tr"把台词中的\\N替换为\\N{\\fnSource Han Sans JP Bold\\fs50\\fsvp10}"
 script_author = "谢耳朵w"
-script_version = "0.2"
+script_version = "0.3"
 
 re = require 'aegisub.re'
-exp = re.compile('\\\\N')
-exp_exist = re.compile('\\\\N{\\\\fnSource Han Sans JP Bold\\\\fs50\\\\fsvp10}')
+exp = re.compile('\\\\+N+')
+exp_skip = re.compile('\\\\N{\\\\fnSource Han Sans JP Bold\\\\fs50\\\\fsvp10}')
+
+function linenum_offset(subs)
+    offset = 0
+    for i = 1, #subs do
+        if subs[i].class ~= "dialogue" then
+            offset = i
+        else
+            break
+        end
+    end
+    return offset
+end
 
 function insert_sep(subs, sels, curr)
+    offset = linenum_offset(subs)
     normalsels = {}
     errsels = {}
     for _, i in ipairs(sels) do
         local line = subs[i]
-        if not exp_exist:match(line.text) then
-            t = exp:sub(line.text, '\\\\N{\\\\fnSource Han Sans JP Bold\\\\fs50\\\\fsvp10}')
-            if t == line.text then
-                -- not found any \N
+        if not exp_skip:match(line.text) then
+            matches = exp:find(line.text)
+            if #matches > 1 then
+                -- found multiple '\N' like
                 table.insert(errsels, i)
                 line.comment = true
             else
-                table.insert(normalsels, i)
+                t = exp:sub(line.text, '\\\\N{\\\\fnSource Han Sans JP Bold\\\\fs50\\\\fsvp10}')
+                if t == line.text then
+                    -- not found any '\N' like
+                    table.insert(errsels, i)
+                    line.comment = true
+                else
+                    table.insert(normalsels, i)
+                end
+                line.text = t
             end
-            line.text = t
             subs[i] = line
         end
     end
-    out = ""
+    out = "Format errors:\n"
     if #errsels > 0 then
         for _, i in ipairs(errsels) do
-            out = out .. i .. ": " .. subs[i].text .. "\n"
+            out = out .. i-offset .. ": " .. subs[i].text .. "\n"
         end
         aegisub.debug.out(out)
         aegisub.debug.out('These lines will be selected and commented out!')
